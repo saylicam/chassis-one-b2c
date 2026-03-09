0b441e784e5a636742e2d1da23324b65de4401ed
Ilyas Nassiri <nassiriilyase@gmail.com>
2026-03-09 18:09:59 +0100

    go

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
